3b0778cf93403e3e62cc9834ee9f7d4c0e5deed3
Dick Blair <major.dick.blair@gmail.com>
2018-10-03 01:14:27 -0500

    Basic Form Structure Created

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benny
+ * Date: 10/3/2018
+ * Time: 12:13 AM
+ */
---
+++ results_page.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benny
+ * Date: 10/3/2018
+ * Time: 12:43 AM
+ */
+ echo('I made it to the results page');
